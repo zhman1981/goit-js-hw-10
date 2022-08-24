dce646858891b5395a5f571529a2fdbcc265ff7c
zhman1981 <zhman@ukr.net>
2022-08-24 15:32:29 +0300

    fix export

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,5 @@
-export default function fetchCountries(name) {
+export const fetchCountries = (name) => {
+// export default function fetchCountries(name) {
      return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
          .then(response => { 
              if (response.ok) {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries.js';
+import { fetchCountries } from './fetchCountries.js';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
