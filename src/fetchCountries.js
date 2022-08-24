3354ee194bce5db240a76f05d9b546de6c53e8fe
zhman1981 <zhman@ukr.net>
2022-08-24 15:24:02 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-export default fetchCountries = (name) => {
+export default function fetchCountries(name) {
      return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
          .then(response => { 
              if (response.ok) {
